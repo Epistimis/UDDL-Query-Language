46916bdb44cfe04375faccea70ff8e0b20882ef7
Steve Hickman <steve@epistimis.com>
2023-11-30 14:39:35 -0800

    Ignore generated test files

--- resig.sh
+++ resig.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 git restore com.epistimis.uddl.query.ide/src-gen/.gitignore
 git restore com.epistimis.uddl.query.ide/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.tests/src-gen/.gitignore
+git restore com.epistimis.uddl.query.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.ui.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.query.ui.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.ui/src-gen/.gitignore
