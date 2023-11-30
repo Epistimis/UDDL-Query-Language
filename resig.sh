fac5b2792754d3af84b5609d2af99cb4a76386e3
Steve Hickman <steve@epistimis.com>
2023-11-29 16:22:51 -0800

    Added shell script to restore gitignore files after Maven deletes them (we need them to force directory creation)
    This runs 'git status' after restoring them. If there are still .gitignores that remain deleted, update this script to restore them

---
+++ resig.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+git restore com.epistimis.uddl.query.ide/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ide/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.ui.tests/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ui.tests/xtend-gen/.gitignore
+git restore com.epistimis.uddl.query.ui/src-gen/.gitignore
+git restore com.epistimis.uddl.query/src-gen/.gitignore
+git status
