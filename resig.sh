845aa1c9056b7ea82e6189520591f14fbd17ed4e
Steve Hickman <steve@epistimis.com>
2023-12-20 11:44:48 -0800

    Another .gitignore - to force xtend-gen creation

--- resig.sh
+++ resig.sh
@@ -7,6 +7,7 @@ git restore com.epistimis.uddl.query.ui.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.query.ui.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.ui/src-gen/.gitignore
 git restore com.epistimis.uddl.query/src-gen/.gitignore
+git restore com.epistimis.uddl.query/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.web/src-gen/.gitignore
 git restore com.epistimis.uddl.query.web/xtend-gen/.gitignore
 git status
