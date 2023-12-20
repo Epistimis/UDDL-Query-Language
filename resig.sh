399dca0c623a99343b585c22a16cff93fb494c7d
Steve Hickman <steve@epistimis.com>
2023-12-20 11:49:32 -0800

    Yet another directory forced to exist via .gitignore

--- resig.sh
+++ resig.sh
@@ -6,6 +6,7 @@ git restore com.epistimis.uddl.query.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.ui.tests/src-gen/.gitignore
 git restore com.epistimis.uddl.query.ui.tests/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.ui/src-gen/.gitignore
+git restore com.epistimis.uddl.query.ui/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query/src-gen/.gitignore
 git restore com.epistimis.uddl.query/xtend-gen/.gitignore
 git restore com.epistimis.uddl.query.web/src-gen/.gitignore
